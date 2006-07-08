10c3ca0b28899665088ffd80ed02aaf40be75702
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2006-07-08 23:09:32 +0000

    fd_dup
    git-svn-id: file:///source/coreland/corelib/trunk@645 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- fd.h
+++ fd.h
@@ -1,6 +1,8 @@
 #ifndef FD_H
 #define FD_H
 
+#include "_fd.h"
+
 int fd_dup(int, int);
 int fd_move(int, int);
 int fd_reset(int);
--- fd_dup.c
+++ fd_dup.c
@@ -5,8 +5,12 @@
 int fd_dup(int src, int dst)
 {
   if (src == dst) return 0;
+#ifdef HAVE_DUP2
+  return (dup2(src, dst) != -1) ? 1 : -1;
+#else
   if (fcntl(src, F_GETFL, 0) == -1) return -1;
   close(dst);
   if (fcntl(src, F_DUPFD, dst) == -1) return -1;
+#endif
   return 1;
 }
