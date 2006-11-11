a303a18abc575692e743db68c61c7f7d926dec01
markzero_svn <markzero_svn@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2006-11-11 21:02:31 +0000

    corelib-conf updates
    git-svn-id: file:///source/coreland/corelib/trunk@1229 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- corelib-conf.c
+++ corelib-conf.c
@@ -8,7 +8,7 @@ const char progname[] = "corelib-config";
 
 void usage()
 {
-  syserr_warn2x(progname, ": usage: [-ILhnsV]");
+  syserr_warn2x(progname, ": usage: [-ILchnsV]");
 }
 void help()
 {
@@ -22,60 +22,55 @@ void help()
 "  -n: print trailing newline");
 }
 
+#define FLAG_INCDIR  0x0001
+#define FLAG_LIBDIR  0x0002
+#define FLAG_NEWLINE 0x0004
+#define FLAG_VER     0x0008
+#define FLAG_COMP    0x0010
+#define FLAG_STATIC  0x0020
+
 int main(int argc, char *argv[])
 {
-  int flag_incdir;
-  int flag_libdir;
-  int flag_nl;
-  int flag_ver;
-  int flag_comp;
-  int flag_static;
+  unsigned int flag;
   int ch;
-
-  flag_comp = 0;
-  flag_ver = 0;
-  flag_nl = 0;
-  flag_incdir = 0;
-  flag_libdir = 0;
-  flag_static = 0;
 
   if (argc < 2) { usage(); return 111; }
 
+  flag = 0;
   while ((ch = get_opt(argc, argv, "ILVchns")) != opteof)
     switch (ch) {
-      case 'I': flag_incdir = 1; break;
-      case 'L': flag_libdir = 1; break;
-      case 'c': flag_comp = 1; break;
+      case 'I': flag |= FLAG_INCDIR; break;
+      case 'L': flag |= FLAG_LIBDIR; break;
+      case 'c': flag |= FLAG_COMP; break;
       case 'h': usage(); help(); return 0; break;
-      case 'n': flag_nl = 1; break;
-      case 's': flag_static = 1; break;
-      case 'V': flag_ver = 1; break;
+      case 'n': flag |= FLAG_NEWLINE; break;
+      case 's': flag |= FLAG_STATIC; break;
+      case 'V': flag |= FLAG_VER; break;
        default: usage(); return 111; break;
     }
 
-  if (flag_ver) {
+  if (flag & FLAG_VER) {
     buffer_puts(buffer1, ctxt_version);
     buffer_puts(buffer1, " ");
   }
-  if (flag_incdir) {
-    if (flag_comp) buffer_puts(buffer1, "-I");
-    buffer_puts(buffer1, ctxt_incdir);
-    if (flag_comp) {
-      buffer_puts(buffer1, " ");
+  if (flag & FLAG_INCDIR) {
+    if (flag & FLAG_COMP) {
       buffer_puts(buffer1, "-I");
       buffer_put(buffer1, ctxt_incdir, str_rchr(ctxt_incdir, '/'));
       buffer_puts(buffer1, " ");
+    } else {
+      buffer_puts(buffer1, ctxt_incdir);
     }
   }
-  if (flag_libdir) {
-    if (flag_comp) buffer_puts(buffer1, "-L");
-    if (flag_static)
+  if (flag & FLAG_LIBDIR) {
+    if (flag & FLAG_COMP) buffer_puts(buffer1, "-L");
+    if (flag & FLAG_STATIC)
       buffer_puts(buffer1, ctxt_slibdir);
     else
       buffer_puts(buffer1, ctxt_dlibdir);
     buffer_puts(buffer1, " ");
   }
-  if (flag_nl) {
+  if (flag & FLAG_NEWLINE) {
     buffer_puts(buffer1, "\n");
   }
   if (buffer_flush(buffer1) == -1)
