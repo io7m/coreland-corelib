068e20d262350e004651f58218842c0d837ddc92
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2006-12-15 02:50:53 +0000

    dir_array missing things
    git-svn-id: file:///source/coreland/corelib/trunk@1317 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- dir_array.h
+++ dir_array.h
@@ -12,8 +12,10 @@ int dir_array_init(struct dir_array *, const char *);
 int dir_array_next(struct dir_array *, char **);
 
 void dir_array_rewind(struct dir_array *);
-void dir_array_free(struct dir_array *);
-void dir_array_dump(struct dir_array *);
 void dir_array_setcmp(struct dir_array *, int (*)(const char *, const char *));
+void dir_array_sort(struct dir_array *);
+void dir_array_free(struct dir_array *);
+
+void dir_array_dump(struct dir_array *);
 
 #endif
