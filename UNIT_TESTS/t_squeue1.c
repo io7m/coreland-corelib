c994279d201cccb65e0ad2f769108c3044d209dc
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-01-01 01:13:01 +0000

    t_squeue
    git-svn-id: file:///source/coreland/corelib/trunk@1377 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- UNIT_TESTS/t_squeue1.c
+++ UNIT_TESTS/t_squeue1.c
@@ -1,6 +1,6 @@
 #include "../squeue.h"
 
-#define QUEUE_SIZE 4
+#define QUEUE_SIZE 8
 
 struct object {
   unsigned long num;
@@ -8,6 +8,11 @@ struct object {
 
 char buf[QUEUE_SIZE * sizeof(struct object)];
 struct squeue sq;
+
+void dump()
+{
+  printf("%lu %lu %lu\n", sq.hpos, sq.tpos, sq.u);
+}
 
 int main()
 {
@@ -19,6 +24,9 @@ int main()
 
   squeue_init(&sq, buf, QUEUE_SIZE, sizeof(struct object));
 
+  printf("h t u\n");
+  dump();
+
   /* check size is zero */
   if (squeue_size(&sq) != 0) {
     printf("fail: squeue_size: %lu != 0\n", squeue_size(&sq));
@@ -29,14 +37,19 @@ int main()
     return 1;
   }
 
+  printf("\n");
+
   /* check enq works */
   for (num = 0; num < QUEUE_SIZE; ++num) {
-    obj.num = num << 8;
+    obj.num = num << 1;
     if (!squeue_enq(&sq, &obj)) {
       printf("fail: squeue_enq: could not add ind %lu\n", num);
       return 1;
     }
+    dump();
   }
+
+  printf("\n");
 
   /* check size is correct */
   if (squeue_bytes(&sq) != QUEUE_SIZE * sizeof(struct object)) {
@@ -64,6 +77,9 @@ int main()
     return 1;
   }
 
+  dump();
+  printf("\n");
+
   /* check deq works */
   for (num = 0; num < QUEUE_SIZE; ++num) {
     if (!squeue_peek(&sq, (void **) &obp)) {
@@ -71,7 +87,7 @@ int main()
       return 1;
     }
     cmp = obp->num;
-    if (cmp != num << 8) {
+    if (cmp != num << 1) {
       printf("fail: squeue_peek: %lu != %lu\n", cmp, num);
       return 1;
     }
@@ -80,17 +96,89 @@ int main()
       return 1;
     }
     cmp = obp->num;
-    if (cmp != num << 8) {
+    if (cmp != num << 1) {
       printf("fail: squeue_deq: %lu != %lu\n", cmp, num);
       return 1;
     }
+    dump();
   }
+
+  printf("\n");
 
   /* check deny underflow */
   if (squeue_deq(&sq, 0)) {
     printf("fail: queue underflow\n");
     return 1;
   }
+
+  dump();
+  printf("\n");
+
+  /* check enq works */
+  for (num = 0; num < QUEUE_SIZE; ++num) {
+    obj.num = num << 1;
+    if (!squeue_enq(&sq, &obj)) {
+      printf("fail: squeue_enq: could not add ind %lu\n", num);
+      return 1;
+    }
+    dump();
+  }
+
+  printf("\n");
+
+  /* check size is correct */
+  if (squeue_bytes(&sq) != QUEUE_SIZE * sizeof(struct object)) {
+    printf("fail: squeue_bytes: %lu != %lu\n", squeue_bytes(&sq),
+            QUEUE_SIZE * sizeof(struct object));
+    return 1;
+  }
+  if (squeue_BYTES(&sq) != QUEUE_SIZE * sizeof(struct object)) {
+    printf("fail: squeue_bytes: %lu != %lu\n", squeue_BYTES(&sq),
+            QUEUE_SIZE * sizeof(struct object));
+    return 1;
+  }
+  if (squeue_size(&sq) != QUEUE_SIZE) {
+    printf("fail: squeue_size: %lu != %lu\n", squeue_size(&sq), QUEUE_SIZE);
+    return 1;
+  }
+  if (squeue_SIZE(&sq) != QUEUE_SIZE) {
+    printf("fail: squeue_SIZE: %lu != %lu\n", squeue_SIZE(&sq), QUEUE_SIZE);
+    return 1;
+  }
+
+  /* check deny overflow */
+  if (squeue_enq(&sq, 0)) {
+    printf("fail: queue overflow\n");
+    return 1;
+  }
+
+  dump();
+  printf("\n");
+
+  /* check deq works */
+  for (num = 0; num < QUEUE_SIZE; ++num) {
+    if (!squeue_peek(&sq, (void **) &obp)) {
+      printf("fail: squeue_peek: could not get ind %lu\n", num);
+      return 1;
+    }
+    cmp = obp->num;
+    if (cmp != num << 1) {
+      printf("fail: squeue_peek: %lu != %lu\n", cmp, num);
+      return 1;
+    }
+    if (!squeue_deq(&sq, (void **) &obp)) {
+      printf("fail: squeue_deq: could not get ind %lu\n", num);
+      return 1;
+    }
+    cmp = obp->num;
+    if (cmp != num << 1) {
+      printf("fail: squeue_deq: %lu != %lu\n", cmp, num);
+      return 1;
+    }
+    dump();
+  }
+
+  printf("\n");
 
   /* check data works */
   vp = squeue_data(&sq);
