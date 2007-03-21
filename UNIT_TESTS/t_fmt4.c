63752619a053a4d331aeda3ab0ce64f34f07f7d4
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-03-21 02:02:01 +0000

    added notes
    fmt_float base
    
    t_fmt4 test
    
    updated TODO
    
    update scan_spec.h
    
    
    
    git-svn-id: file:///source/coreland/corelib/trunk@1940 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- UNIT_TESTS/t_fmt4.c
+++ UNIT_TESTS/t_fmt4.c
@@ -5,32 +5,51 @@
 struct fmt_test {
   float num;
   char *str;
-  unsigned int prec;
+  unsigned int sig;
+  unsigned int len;
 };
 
 static const struct fmt_test tests[] = {
-  { 0.0, "0.000000", 0 },
-  { 0.0, "0.0",      1 },
-  { 0.0, "0.00",     2 },
-  { 0.0, "0.000",    3 },
-  { 0.0, "0.0000",   4 },
-  { 0.0, "0.00000",  5 },
-  { 0.0, "0.000000", 6 },
-  { -0.0, "-0.000000", 0 },
-  { -1.0, "-0.100000", 0 },
+  { 0.0,      "",          0, 0 },
+  { 0.0,      "0",         1, 1 },
+  { 0.0,      "0.0",       2, 3 },
+  { 0.0,      "0.00",      3, 4 },
+  { 0.0,      "0.000",     4, 5 },
+  { 0.0,      "0.0000",    5, 6 },
+  { 0.0,      "0.00000",   6, 7 },
+  { -0.0,     "-0.000000", 7, 9 },
+  { -1.0,     "-0.100000", 7, 9 },
+  { 1.23456,  "1.23456",   6, 7 },
+  { 1.2345,   "1.2345",    5, 6 },
+  { 1.234,    "1.234",     4, 5 },
+  { 1.23,     "1.23",      3, 4 },
+  { 1.2,      "1.2",       2, 3 },
+  { 1,        "1",         1, 1 },
 };
 
 int main(void)
 {
   char cnum[FMT_FLOAT];
   unsigned int ind;
+  unsigned int len;
 
   for (ind = 0; ind < sizeof(tests) / sizeof(struct fmt_test); ++ind) {
-    cnum[fmt_float(cnum, tests[ind].num, tests[ind].prec)] = 0;
+    len = fmt_float(FMT_LEN, tests[ind].num, tests[ind].sig);
+    if (len != tests[ind].len) {
+      printf("fail: [%u] FMT_LEN len %u != %u\n", ind, len, tests[ind].len);
+      return 1;
+    }
+    len = fmt_float(cnum, tests[ind].num, tests[ind].sig);
+    if (len != tests[ind].len) {
+      printf("fail: [%u] len %u != %u\n", ind, len, tests[ind].len);
+      return 1;
+    }
+    cnum[len] = 0;
     if (!str_same(cnum, tests[ind].str)) {
       printf("fail: [%u] %s != %s\n", ind, cnum, tests[ind].str);
       return 1;
     }
+    printf("[%lu] %s == %s\n", ind, tests[ind].str, cnum);
   }
 
   return 0;
--- fmt_float.c
+++ fmt_float.c
@@ -1,11 +1,63 @@
 #include <float.h>
+#include <math.h>
 #include "fmt.h"
 
-unsigned int fmt_float(char *str, float f, unsigned int prec)
+/* IEEE 754 single precision only */
+
+#define FLOAT_SIGN_BITS     31
+#define FLOAT_MANTISSA_BITS 23
+#define FLOAT_EXPONENT_MASK 0x0ff00000
+
+unsigned int fmt_float(char *str, float f, unsigned int rnd)
 {
   unsigned int len = 0;
+  union {
+    float f;
+    unsigned long u;
+  } real;
+  signed long expo;
+  signed long mant;
+  signed long sign;
+  unsigned long ind;
 
-  if (!prec || prec > FLT_DIG) prec = FLT_DIG;
+  if (!rnd) return 0;
+
+  real.f = f;
+  sign = real.u >> FLOAT_SIGN_BITS;
+  expo = real.u >> FLOAT_MANTISSA_BITS;
+  mant = real.u & FLOAT_EXPONENT_MASK;
+
+  /* format string */
+  if (sign) {
+    ++len;
+    if (str) *str++ = '-';
+  }
 
   return len;
 }
+
+/*
+  if (!expo && !mant) {
+    if (sign) {
+      if (str) *str++ = '-';
+      ++len;
+    }
+    if (rnd > 1) {
+      if (str) {
+        str[0] = '0';
+        str[1] = '.';
+        str += 2;
+      }
+      len += 2;
+      rnd -= 1;
+      for (ind = 0; ind < rnd; ++ind) {
+        if (str) *str++ = '0';
+        ++len;
+      }
+      return len;
+    } else {
+      if (str) *str = '0';
+      return 1;
+    }
+  }
+*/
