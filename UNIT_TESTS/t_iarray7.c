599a25c2c6e8d701ffe0873f51c0f237eaa762ba
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-03-22 08:03:35 +0000

    iarray_sort fixed.
    git-svn-id: file:///source/coreland/corelib/trunk@1958 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- UNIT_TESTS/t_iarray7.c
+++ UNIT_TESTS/t_iarray7.c
@@ -9,6 +9,13 @@ int sort_numeric(const void *ap, unsigned long asize,
   unsigned long b = * (unsigned long *) bp;
   return (a - b);
 }
+int sort_numeric_rev(const void *ap, unsigned long asize,
+                     const void *bp, unsigned long bsize)
+{
+  unsigned long a = * (unsigned long *) ap;
+  unsigned long b = * (unsigned long *) bp;
+  return (b - a);
+}
 
 struct iarray ia;
 struct iarray_node *nptr;
@@ -49,14 +56,144 @@ int test1()
   iarray_free(&ia);
 }
 
+/* reverse numeric odd -> numeric odd */
+int test2()
+{
+  printf("--\n");
+
+  if (!iarray_init(&ia, 10)) {
+    printf("fail: iarray_init\n");
+    return 1;
+  }
+  for (ind = 10; ind; --ind) {
+    if (!iarray_cat(&ia, &ind, sizeof(unsigned long))) {
+      printf("fail: iarray_cat %lu\n", ind);
+      return 0;
+    }
+  }
+  for (ind = 10; ind; --ind)
+    if (ind % 2 == 0)
+      iarray_remove(&ia, ind);
+ 
+  iarray_dump(&ia, IARRAY_DUMP_INT);
+  iarray_sort(&ia, sort_numeric);
+  iarray_dump(&ia, IARRAY_DUMP_INT);
+  if (!iarray_integrity(&ia)) return 0;
+
+  nptr = ia.head;
+  for (ind = 0; ind < 5; ++ind) {
+    data = * (unsigned long *) nptr->data;
+    if (data != ind * 2 + 1) {
+      printf("[%lu] fail: expected %lu got %lu\n", ind, ind * 2 + 1, data);
+      return 0;
+    }
+    nptr = nptr->next;
+    if (!nptr) break;
+  }
+
+  iarray_free(&ia);
+}
+
+/* reverse numeric -> reverse numeric */
+int test3()
+{
+  printf("--\n");
+
+  if (!iarray_init(&ia, 10)) {
+    printf("fail: iarray_init\n");
+    return 1;
+  }
+  for (ind = 10; ind; --ind) {
+    if (!iarray_cat(&ia, &ind, sizeof(unsigned long))) {
+      printf("fail: iarray_cat %lu\n", ind);
+      return 0;
+    }
+  }
+
+  iarray_dump(&ia, IARRAY_DUMP_INT);
+  iarray_sort(&ia, sort_numeric_rev);
+  iarray_dump(&ia, IARRAY_DUMP_INT);
+  if (!iarray_integrity(&ia)) return 0;
+
+  nptr = ia.head;
+  for (ind = 0; ind < 10; ++ind) {
+    data = * (unsigned long *) nptr->data;
+    if (data != 10 - ind) {
+      printf("[%lu] fail: expected %lu got %lu\n", ind, 10 - ind, data);
+      return 0;
+    }
+    nptr = nptr->next;
+    if (!nptr) break;
+  }
+
+  iarray_free(&ia);
+}
+
+int test4()
+{
+  unsigned long unsorted[] = {
+    49130,
+    16616,
+    77590,
+    18428,
+    5456,
+    59622,
+    46486,
+    64587,
+    71369,
+    1574,
+  };
+  unsigned long sorted[] = {
+    1574,
+    5456,
+    16616,
+    18428,
+    46486, 
+    49130,
+    59622,
+    64587, 
+    71369,
+    77590,
+  };
+
+  printf("--\n");
+
+  if (!iarray_init(&ia, 10)) {
+    printf("fail: iarray_init\n");
+    return 1;
+  }
+  for (ind = 0; ind < sizeof(unsorted) / sizeof(unsorted[0]); ++ind) {
+    if (!iarray_cat(&ia, &unsorted[ind], sizeof(unsigned long))) {
+      printf("fail: iarray_cat %lu\n", ind);
+      return 0;
+    }
+  }
+
+  iarray_dump(&ia, IARRAY_DUMP_INT);
+  iarray_sort(&ia, sort_numeric);
+  iarray_dump(&ia, IARRAY_DUMP_INT);
+  if (!iarray_integrity(&ia)) return 0;
+
+  nptr = ia.head;
+  for (ind = 0; ind < sizeof(sorted) / sizeof(sorted[0]); ++ind) {
+    data = * (unsigned long *) nptr->data;
+    if (data != sorted[ind]) {
+      printf("[%lu] fail: expected %lu got %lu\n", ind, sorted[ind], data);
+      return 0;
+    }
+    nptr = nptr->next;
+    if (!nptr) break;
+  }
+
+  iarray_free(&ia);
+}
+
 int main()
 {
   if (!test1()) return 1;
-
-  {
-    printf("fail: IARRAY SORTING IS BROKEN!\n");
-    return 1;
-  }
+  if (!test2()) return 1;
+  if (!test3()) return 1;
+  if (!test4()) return 1;
 
   return 0;
 }
--- iarray_sort.c
+++ iarray_sort.c
@@ -48,6 +48,7 @@ void iarray_sort(struct iarray *ia, int (*cmp)(const void *, unsigned long,
           enext = subl1; subl1 = subl1->next; --subl1_len;
           goto ADD_ELEMENT;
         }
+
         if (!subl2->data) {
           if (!subl1->data) {
             enext = subl2; subl2 = subl2->next; --subl2_len;
@@ -62,7 +63,7 @@ void iarray_sort(struct iarray *ia, int (*cmp)(const void *, unsigned long,
             enext = subl1; subl1 = subl1->next; --subl1_len;
             goto ADD_ELEMENT;
           } else {
-            enext = subl1; subl1 = subl1->next; --subl1_len;
+            enext = subl2; subl2 = subl2->next; --subl2_len;
             goto ADD_ELEMENT;
           }
         }
