e4a8356f15d4f13901498712dc4154a05ef31ce9
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-04-11 10:53:08 +0000

    syserr takes eight arguments
    git-svn-id: file:///source/coreland/corelib/trunk@2129 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- syserr_die.c
+++ syserr_die.c
@@ -2,8 +2,8 @@
 #include "exit.h"
 #include "syserr.h"
 
-void syserr_warn(const char *x1, const char *x2, const char *x3, 
-                 const char *x4, const char *x5, const char *x6,
+void syserr_warn(const char *x1, const char *x2, const char *x3, const char *x4,
+                 const char *x5, const char *x6, const char *x7, const char *x8,
                  struct syserr *se)
 {
   syserr_init();
@@ -14,6 +14,8 @@ void syserr_warn(const char *x1, const char *x2, const char *x3,
   if (x4) buffer_puts(buffer2, x4);
   if (x5) buffer_puts(buffer2, x5);
   if (x6) buffer_puts(buffer2, x6);
+  if (x7) buffer_puts(buffer2, x7);
+  if (x8) buffer_puts(buffer2, x8);
  
   while(se) {
     if (se->x) buffer_puts(buffer2, se->x);
@@ -26,10 +28,10 @@ void syserr_warn(const char *x1, const char *x2, const char *x3,
   buffer_flush(buffer2);
 }
 
-void syserr_die(int e, const char *x1, const char *x2, const char *x3, 
-                       const char *x4, const char *x5, const char *x6,
+void syserr_die(int e, const char *x1, const char *x2, const char *x3, const char *x4,
+                       const char *x5, const char *x6, const char *x7, const char *x8,
                        struct syserr *se)
 {
-  syserr_warn(x1, x2, x3, x4, x5, x6, se);
+  syserr_warn(x1, x2, x3, x4, x5, x6, x7, x8, se);
   _exit(e);
 }
