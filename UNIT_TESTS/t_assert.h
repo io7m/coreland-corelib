9799378382c65310dd956ffac810fdcfab68b191
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-05-04 18:49:47 +0000

    MipsPro C < 7.4.0 doesn't have __func__
    git-svn-id: file:///source/coreland/corelib/trunk@2244 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- UNIT_TESTS/t_assert.h
+++ UNIT_TESTS/t_assert.h
@@ -1,8 +1,13 @@
 #ifndef T_ASSERT_H
 #define T_ASSERT_H
 
-#define test_assert(e) \
-  test_assert_core((e) ? 1 : 0, __func__, __LINE__, #e)
+#if defined(__func__)
+  #define test_assert(e) \
+    test_assert_core((e) ? 1 : 0, __func__, __LINE__, #e)
+#else
+  #define test_assert(e) \
+    test_assert_core((e) ? 1 : 0, "", __LINE__, #e)
+#endif
 
 void test_assert_core(unsigned int, const char *, unsigned long, const char *);
 
