84fe457c407dd0b643d0b3a8b43f6e35dcc65d7e
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-05-06 18:28:41 +0000

    removed test for __func__
    git-svn-id: file:///source/coreland/corelib/trunk@2289 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- UNIT_TESTS/t_assert.h
+++ UNIT_TESTS/t_assert.h
@@ -1,13 +1,8 @@
 #ifndef T_ASSERT_H
 #define T_ASSERT_H
 
-#if defined(__func__)
-  #define test_assert(e) \
-    test_assert_core((e) ? 1 : 0, __func__, __LINE__, #e)
-#else
-  #define test_assert(e) \
-    test_assert_core((e) ? 1 : 0, "", __LINE__, #e)
-#endif
+#define test_assert(e) \
+  test_assert_core((e) ? 1 : 0, __func__, __LINE__, #e)
 
 void test_assert_core(unsigned int, const char *, unsigned long, const char *);
 
