b1045f38246baf327df457a5b292bfd31c62d4f3
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-07-08 01:42:51 +0000

    dir_walk uses dir_array for deterministic, sorted directories.
    git-svn-id: file:///source/coreland/corelib/trunk@2723 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- dir_walk.c
+++ dir_walk.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 #include "bin.h"
-#include "direntry.h"
+#include "dir_array.h"
 #include "open.h"
 #include "str.h"
 #include "sstring.h"
@@ -11,23 +11,18 @@
 
 int dir_func(struct dir_walk *dw, const char *dn)
 {
+  struct dir_array da = {0, 0, 0, 0};
   struct stat sb;
-  DIR *dir;
-  direntry *de;
   unsigned long pos;
+  char *name;
   int pwdfd;
-  int ret;
-
-  dir = 0;
-  ret = 1;
+  int ret = 1;
 
   pwdfd = open_ro(".");
   if (pwdfd == -1) return 0;  
   if (chdir(dn) == -1) { ret = 0; goto END; }
 
-  dir = opendir(".");
-  if (!dir) { ret = 0; goto END; }
-
+  if (!dir_array_init(&da, ".")) { ret = 0; goto END; }
   if (dw->path.len) 
     sstring_catb(&dw->path, "/", 1);
 
@@ -35,39 +30,38 @@ int dir_func(struct dir_walk *dw, const char *dn)
   sstring_0(&dw->path);
 
   for (;;) {
-    de = readdir(dir);
-    if (!de) break;
-    if (!str_same(de->d_name, ".") && !str_same(de->d_name, "..")) {
-      if (lstat(de->d_name, &sb) == -1) continue; 
+    if (!dir_array_next(&da, &name)) break;
+    if (!str_same(name, ".") && !str_same(name, "..")) {
+      if (lstat(name, &sb) == -1) continue; 
       switch (sb.st_mode & S_IFMT) {
         case S_IFIFO:
           if (dw->cb_fifo)
-            if (!dw->cb_fifo(de->d_name, dw->path.s, dw->data)) goto END;
+            if (!dw->cb_fifo(name, dw->path.s, dw->data)) goto END;
           break;
         case S_IFCHR:
           if (dw->cb_chr)
-            if (!dw->cb_chr(de->d_name, dw->path.s, dw->data)) goto END;
+            if (!dw->cb_chr(name, dw->path.s, dw->data)) goto END;
           break;
         case S_IFDIR:
           if (dw->cb_dir)
-            if (!dw->cb_dir(de->d_name, dw->path.s, dw->data)) goto END;
-          if (!dir_func(dw, de->d_name)) goto END;
+            if (!dw->cb_dir(name, dw->path.s, dw->data)) goto END;
+          if (!dir_func(dw, name)) goto END;
           break;
         case S_IFBLK:
           if (dw->cb_blk)
-            if (!dw->cb_blk(de->d_name, dw->path.s, dw->data)) goto END;
+            if (!dw->cb_blk(name, dw->path.s, dw->data)) goto END;
           break;
         case S_IFREG:
           if (dw->cb_reg)
-            if (!dw->cb_reg(de->d_name, dw->path.s, dw->data)) goto END;
+            if (!dw->cb_reg(name, dw->path.s, dw->data)) goto END;
           break;
         case S_IFLNK:
           if (dw->cb_lnk)
-            if (!dw->cb_lnk(de->d_name, dw->path.s, dw->data)) goto END;
+            if (!dw->cb_lnk(name, dw->path.s, dw->data)) goto END;
           break;
         case S_IFSOCK: 
           if (dw->cb_sock)
-            if (!dw->cb_sock(de->d_name, dw->path.s, dw->data)) goto END;
+            if (!dw->cb_sock(name, dw->path.s, dw->data)) goto END;
           break;
         default:
           break;
@@ -85,7 +79,7 @@ int dir_func(struct dir_walk *dw, const char *dn)
     fchdir(pwdfd);
     close(pwdfd);
   }
-  if (dir) closedir(dir);
+  if (da.a) dir_array_free(&da);
   return ret;
 }
 
