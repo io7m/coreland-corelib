425dc87101ae84db5e311a3a6c64982a8e8dfb2f
m0 <m0@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2008-02-23 18:41:04 +0000

    20080223 code: removed sysinfo.c, included _sysinfo.h in corelib-conf.c
    git-svn-id: file:///source/coreland/corelib/trunk@2964 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- insthier.c
+++ insthier.c
@@ -249,7 +249,6 @@ struct install_item insthier[] = {
   {INST_COPY, "str_toupper.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "syserr_die.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "syserr_init.c", 0, ctxt_repos, 0, 0, 0644},
-  {INST_COPY, "sysinfo.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "_direntry.h", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "_direntry.h", 0, ctxt_incdir, 0, 0, 0644},
   {INST_COPY, "_sig_action.h", 0, ctxt_repos, 0, 0, 0644},
@@ -416,7 +415,5 @@ struct install_item insthier[] = {
   {INST_COPY, "syserr.a", "libsyserr.a", ctxt_slibdir, 0, 0, 0644},
   {INST_COPY, "corelib-conf.ld", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "corelib-conf", 0, ctxt_bindir, 0, 0, 0755},
-  {INST_COPY, "sysinfo.ld", 0, ctxt_repos, 0, 0, 0644},
-  {INST_COPY, "sysinfo", 0, ctxt_bindir, 0, 0, 0755},
 };
 unsigned long insthier_len = sizeof(insthier) / sizeof(struct install_item);
--- sysinfo.c
+++
@@ -1,6 +0,0 @@
-#include "_sysinfo.h"
-
-int main(void)
-{
-  return 0;
-}
